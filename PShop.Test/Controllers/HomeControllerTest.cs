a0e420a3c6c41a7db5806e6fe4f1f9dad7614b96
Zepsen <zepsendm@gmail.com>
2017-04-13 22:15:00 +0300

    Tests

---
+++ PShop.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PShop.Controllers;
+using Xunit;
+
+namespace PShop.Test.Controllers
+{    
+    class HomeControllerTest
+    {
+        [Fact]
+        public void IndexViewNameIndex()
+        {
+            //arrange
+            var ctrl = new HomeController();
+            //act
+            var res = ctrl.Index() as ViewResult;
+            //assert
+            Assert.Equal("Index", res?.ViewName);
+
+        }
+    }
+}
